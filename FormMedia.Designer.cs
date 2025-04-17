52150140efd62d8af7bbb68f92b702bddee075de
Mpripri <mariapriscilasouza3@gmail.com>
2025-04-17 02:02:48 -0300

    Add files via upload

---
+++ FormMedia.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace App2_MediaAluno
+{
+    partial class FormMedia
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            txtNome = new TextBox();
+            txtComponente2 = new TextBox();
+            txtComponente1 = new TextBox();
+            txtNota4C2 = new TextBox();
+            txtNota3C2 = new TextBox();
+            txtNota2C2 = new TextBox();
+            txtNota1C2 = new TextBox();
+            txtNota4C1 = new TextBox();
+            txtNota3C1 = new TextBox();
+            txtNota2C1 = new TextBox();
+            txtNota1C1 = new TextBox();
+            btnCalcular = new Button();
+            txtMedia = new TextBox();
+            lblResultado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(350, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(188, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Componente 1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(188, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 2;
+            label3.Text = "1°Bimestre";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(188, 197);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 15);
+            label4.TabIndex = 3;
+            label4.Text = "2°Bimestre";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(188, 247);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 15);
+            label5.TabIndex = 4;
+            label5.Text = "3°Bimestre";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(188, 290);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 15);
+            label6.TabIndex = 5;
+            label6.Text = "4°Bimestre";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(457, 84);
+            label7.Name = "label7";
+            label7.Size = new Size(86, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Componente 2";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(457, 152);
+            label8.Name = "label8";
+            label8.Size = new Size(64, 15);
+            label8.TabIndex = 7;
+            label8.Text = "1°Bimestre";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(457, 197);
+            label9.Name = "label9";
+            label9.Size = new Size(64, 15);
+            label9.TabIndex = 8;
+            label9.Text = "2°Bimestre";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(457, 247);
+            label10.Name = "label10";
+            label10.Size = new Size(64, 15);
+            label10.TabIndex = 9;
+            label10.Text = "3°Bimestre";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(457, 290);
+            label11.Name = "label11";
+            label11.Size = new Size(64, 15);
+            label11.TabIndex = 10;
+            label11.Text = "4°Bimestre";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(295, 9);
+            label12.Name = "label12";
+            label12.Size = new Size(156, 15);
+            label12.TabIndex = 11;
+            label12.Text = "Calculadora de Média Anual";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(326, 54);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 12;
+            // 
+            // txtComponente2
+            // 
+            txtComponente2.Location = new Point(457, 102);
+            txtComponente2.Name = "txtComponente2";
+            txtComponente2.Size = new Size(100, 23);
+            txtComponente2.TabIndex = 13;
+            // 
+            // txtComponente1
+            // 
+            txtComponente1.Location = new Point(188, 102);
+            txtComponente1.Name = "txtComponente1";
+            txtComponente1.Size = new Size(100, 23);
+            txtComponente1.TabIndex = 14;
+            // 
+            // txtNota4C2
+            // 
+            txtNota4C2.Location = new Point(527, 290);
+            txtNota4C2.Name = "txtNota4C2";
+            txtNota4C2.Size = new Size(30, 23);
+            txtNota4C2.TabIndex = 15;
+            // 
+            // txtNota3C2
+            // 
+            txtNota3C2.Location = new Point(527, 244);
+            txtNota3C2.Name = "txtNota3C2";
+            txtNota3C2.Size = new Size(30, 23);
+            txtNota3C2.TabIndex = 16;
+            // 
+            // txtNota2C2
+            // 
+            txtNota2C2.Location = new Point(527, 197);
+            txtNota2C2.Name = "txtNota2C2";
+            txtNota2C2.Size = new Size(30, 23);
+            txtNota2C2.TabIndex = 17;
+            // 
+            // txtNota1C2
+            // 
+            txtNota1C2.Location = new Point(527, 152);
+            txtNota1C2.Name = "txtNota1C2";
+            txtNota1C2.Size = new Size(30, 23);
+            txtNota1C2.TabIndex = 18;
+            // 
+            // txtNota4C1
+            // 
+            txtNota4C1.Location = new Point(258, 287);
+            txtNota4C1.Name = "txtNota4C1";
+            txtNota4C1.Size = new Size(30, 23);
+            txtNota4C1.TabIndex = 19;
+            // 
+            // txtNota3C1
+            // 
+            txtNota3C1.Location = new Point(258, 244);
+            txtNota3C1.Name = "txtNota3C1";
+            txtNota3C1.Size = new Size(30, 23);
+            txtNota3C1.TabIndex = 20;
+            // 
+            // txtNota2C1
+            // 
+            txtNota2C1.Location = new Point(258, 189);
+            txtNota2C1.Name = "txtNota2C1";
+            txtNota2C1.Size = new Size(30, 23);
+            txtNota2C1.TabIndex = 21;
+            // 
+            // txtNota1C1
+            // 
+            txtNota1C1.Location = new Point(258, 144);
+            txtNota1C1.Name = "txtNota1C1";
+            txtNota1C1.Size = new Size(30, 23);
+            txtNota1C1.TabIndex = 22;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(354, 337);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 23;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // txtMedia
+            // 
+            txtMedia.Location = new Point(350, 382);
+            txtMedia.Name = "txtMedia";
+            txtMedia.Size = new Size(100, 23);
+            txtMedia.TabIndex = 24;
+            // 
+            // iblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(354, 363);
+            lblResultado.Name = "iblResultado";
+            lblResultado.Size = new Size(85, 15);
+            lblResultado.TabIndex = 25;
+            lblResultado.Text = "Resultado final";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResultado);
+            Controls.Add(txtMedia);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtNota1C1);
+            Controls.Add(txtNota2C1);
+            Controls.Add(txtNota3C1);
+            Controls.Add(txtNota4C1);
+            Controls.Add(txtNota1C2);
+            Controls.Add(txtNota2C2);
+            Controls.Add(txtNota3C2);
+            Controls.Add(txtNota4C2);
+            Controls.Add(txtComponente1);
+            Controls.Add(txtComponente2);
+            Controls.Add(txtNome);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private TextBox txtNome;
+        private TextBox txtComponente2;
+        private TextBox txtComponente1;
+        private TextBox txtNota4C2;
+        private TextBox txtNota3C2;
+        private TextBox txtNota2C2;
+        private TextBox txtNota1C2;
+        private TextBox txtNota4C1;
+        private TextBox txtNota3C1;
+        private TextBox txtNota2C1;
+        private TextBox txtNota1C1;
+        private Button btnCalcular;
+        private TextBox txtMedia;
+        private Label lblResultado;
+    }
+}
